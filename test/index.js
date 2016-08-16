91c9d2857695495359d970d19541c87729b00df7
Héctor Menéndez <etor.mx@gmail.com>
2016-08-15 21:43:23 -0500

    Added (repo version) feliz.test for test automation. (cases are still invalid)

--- test/index.js
+++ test/index.js
@@ -1,9 +1,38 @@
 'use strict';
 
-const Tape = require('tape');
-const Server = require('../lib/index');
+const Test = require('feliz.test');
 
-Tape('The feliz-server-hapi module', t => {
-    t.skip('should have tests');
-    t.end();
-})
+const test = new Test([
+    {
+        desc:'The observable plugin',
+        data: [
+            {
+                desc: 'no route file is found',
+                conf: { root: './holamundo' },
+                pass: false
+            },
+            {
+                desc:'I am another test',
+                conf: { root: './adiosmundo' },
+                pass: true
+            }
+        ]
+    },
+    {
+        desc:'The observable plugin 1',
+        data: [
+            {
+                desc: 'no route file is found 1',
+                conf: { root: './holamundo' },
+                pass: false
+            },
+            {
+                desc:'I am another test 1',
+                conf: { root: './adiosmundo' },
+                pass: true
+            }
+        ]
+    }
+]);
+
+test.run();
